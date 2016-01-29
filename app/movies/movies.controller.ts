f9c4d5dda097b09f69efefb58e2cf69ae118803f
Robert Freire <robert.freire@gmail.com>
2016-01-29 07:39:44 +0100

    Calls to a server to get the movies

--- app/movies/movies.controller.ts
+++ app/movies/movies.controller.ts
@@ -7,33 +7,19 @@ namespace app.movies {
         orderProp: string;
     }
 
+    let $inject = ["$http"];
+
     class MoviesController {
-        public movies: app.model.IMovies[];
+        public movies: app.model.IMovies[] = [];
         public orderProp: string;
 
-        constructor() {
-            this.movies = [{
-                overview: "Thirty years after defeating the Galactic Empire, Han Solo and his allies face a new threat from the evil Kylo Ren and his army of Stormtroopers.",
-                title: "Star Wars: The Force Awakens",
-                release_date: new Date("2015-12-18"),
-                popularity: 57.865219,
-                vote_average: 7.84
-            }, {
-                overview: "A cryptic message from Bond’s past sends him on a trail to uncover a sinister organization. While M battles political forces to keep the secret service alive, Bond peels back the layers of deceit to reveal the terrible truth behind SPECTRE.",
-                title: "Spectre",
-                release_date: new Date("2015-10-26"),
-                popularity: 51.83321,
-                vote_average: 6.32
-            }, {
-                overview: "In the 1820s, a frontiersman, Hugh Glass, sets out on a path of vengeance against those who left him for dead after a bear mauling.",
-                title: "The Revenant",
-                release_date: new Date("2015-12-25"),
-                popularity: 44.097331,
-                vote_average: 7.3
-            }];
-
+        constructor(private $http: ng.IHttpService) {
+            let path = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ba7dc0d5812ddda58e32b566e91d4688";
+            this.$http.get(path).success((data) => {
+                this.movies = data.results;
+            });
             this.orderProp = "popularity";
-        }
+        };
     }
 
     angular.module("app.movies")
