1c541a893b4832adda8f5285bef5ccbf89641c29
robert-freire <robert.freire@gmail.com>
2016-06-11 22:14:34 +0200

    Add filters

---
+++ step11/app/movies-list/movies-list.component.ts
@@ -0,0 +1,44 @@
+import {IMovie} from './../model';
+import {IMoviesDataService} from './../shared';
+
+
+export interface IMoviesListController{
+    imagesUrl:string,
+    movies: IMovie[]
+}
+
+class MoviesListController implements IMoviesListController {
+    static $inject: Array<string> = ['imagesUrl', 'moviesdataservice'];
+        
+    public movies: IMovie[];
+
+    constructor (public imagesUrl: string, 
+                private moviesdataservice: IMoviesDataService){
+
+        var vm = this;
+
+        this.moviesdataservice.getList().then ( (movies:IMovie[]) =>{
+            this.movies = movies;
+        });
+    }
+}
+
+/**
+ * @desc component to show a list of movies
+ * @example <movies-list></movies-list>>
+ */
+export class MoviesList implements ng.IComponentOptions{
+    public template = `
+         <span >
+            <label for="search">Search: </label><input ng-model="$ctrl.query" id="search">
+        </span>
+        <ul>
+            <li ng-repeat="movie in $ctrl.movies | filter:$ctrl.query">
+                <img ng-src="{{$ctrl.imagesUrl}}{{movie.poster_path}}" title="{{movie.title}}">
+                <div class="caption"> {{movie.title}} </div>
+            </li>
+        </ul>`;   
+    public controller = MoviesListController;     
+}
+
+                                              
---
+++ step11/app/shared/dataServices/movies.dataService.ts
@@ -0,0 +1,44 @@
+import { IMovie } from './../../model';
+
+/**
+ * DataService o retrieve movies
+ * Detailed information about the API used in http://docs.themoviedb.apiary.io/
+ */
+export interface IMoviesDataService {
+
+    /**
+     * Uri Path to the http server of the images
+     * @type {string}
+     */
+    imagesUrl: string;
+
+    /**
+     * Returns a list of movies
+     * @return {IPromise<IMovie[]>}      [List of movies]
+     */
+    getList():  ng.IPromise<IMovie[]>;
+}
+
+export class MoviesDataService implements IMoviesDataService {
+    static $inject: Array<string> = ['$http', '$q', 'apiKey', 'imagesUrl', 'serverUrl'];
+
+    constructor(
+        private $http: ng.IHttpService,
+        private $q: ng.IQService,
+        private apiKey: string,
+        public imagesUrl: string,
+        private serverUrl: string) {
+    }
+
+    public getList(): ng.IPromise<IMovie[]> {
+        var path = this.serverUrl + 'discover/movie?sort_by=popularity.desc&api_key=' + this.apiKey;
+
+        var deferred = this.$q.defer();
+        this.$http.get(path).success((data: any) => {
+            deferred.resolve(data.results);
+        });
+
+        return deferred.promise;
+    }
+}
+
