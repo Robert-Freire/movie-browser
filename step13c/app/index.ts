8240b8483bcf1bfbb34191aa9661d2b422ac528a
robert-freire <robert.freire@gmail.com>
2016-06-12 09:10:18 +0200

    Add bootstrap

---
+++ step13c/app/index.ts
@@ -0,0 +1,18 @@
+import * as angular from 'angular';
+import { MoviesList } from  './movies-list'; 
+import { MoviesDataService } from './shared';
+
+import '../css/app.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+var moviesBrowserApp: ng.IModule = angular.module('moviesBrowserApp', []); 
+
+
+moviesBrowserApp
+	.component('moviesList', new MoviesList ())
+	.service('moviesdataservice', MoviesDataService)
+	.constant('imagesUrl', 'https://image.tmdb.org/t/p/w185/')
+	.constant('serverUrl', 'http://api.themoviedb.org/3/')
+	.constant('apiKey', 'ba7dc0d5812ddda58e32b566e91d4688');
+
---
+++ step13c/app/movies-list/movies-list.component.ts
@@ -0,0 +1,64 @@
+import {IMoviesDataService, IMovie} from './../shared';
+
+export interface IMoviesListController{
+    imagesUrl:string,
+    movies: IMovie[]
+}
+
+class MoviesListController implements IMoviesListController {
+    static $inject: Array<string> = ['imagesUrl', 'moviesdataservice'];
+        
+    public movies: IMovie[];
+    private currentPage: number;
+
+    constructor (public imagesUrl: string, 
+                private moviesdataservice: IMoviesDataService){
+
+        var vm = this;
+        this.currentPage = 1;
+        this.loadMoviesPage(this.currentPage);
+    }
+
+    public nextPage() {
+        this.loadMoviesPage(++this.currentPage);
+    }
+
+    public previousPage(id: string) {
+        this.loadMoviesPage(--this.currentPage);
+    }
+
+    private loadMoviesPage(page: number) {
+        this.moviesdataservice.getList(this.currentPage).then((movies: IMovie[]) => {
+            this.movies = movies;
+        });
+    }
+}
+
+/**
+ * @desc component to show a list of movies
+ * @example <movies-list></movies-list>>
+ */
+export class MoviesList implements ng.IComponentOptions{
+    public template = `
+         <span >
+            <label for="search">Search: </label><input ng-model="$ctrl.query" id="search">
+            <select ng-model="$ctrl.orderProp" id="sort">
+                <option value="title">Alphabetical</option>
+                <option value="popularity">Popularity</option>
+                <option value="vote_average">Vote</option>                    
+            </select>   
+            <a ng-click="$ctrl.previousPage()" href="#" ><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+            <a ng-click="$ctrl.nextPage()" href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+
+        </span>
+        <div class="movies"> 
+            <ul>
+                <li ng-repeat="movie in $ctrl.movies | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail movie col-lg-3">
+                    <img ng-src="{{$ctrl.imagesUrl}}{{movie.poster_path}}" title="{{movie.title}}">
+                    <div class="caption"> {{movie.title}} </div>
+                </li>
+            </ul>
+        </div>`;   
+    public controller = MoviesListController;     
+}                     
+
