b6d65509ce7493869eb05b908f9320563954bc6d
Robert Freire <robert.freire@gmail.com>
2016-06-13 12:56:51 +0200

    Add test to pagination

--- step11/app/movies-list/movies-list.component.spec.js
+++ step11/app/movies-list/movies-list.component.spec.js
@@ -1,10 +1,32 @@
+"use strict";
+
 describe('MoviesList', function() {
 
   beforeEach(module('moviesBrowserApp'));
 
   describe ('MoviesListController', function (){
-	  it('should create a moviliesList with three elements', inject(function($componentController) {
-	    var ctrl = $componentController('moviesList', {});
+	  it('should create a moviliesList with three elements', inject(function($componentController, $q, $rootScope) {
+
+	  	var moviesDefer = $q.defer();
+
+	  	var moviesDataServiceMock = {
+	  		getList: function (){
+	  			return moviesDefer.promise;
+	  		}
+	  	}
+	    var ctrl = $componentController('moviesList', {
+	    	moviesdataservice: moviesDataServiceMock
+	    });
+
+	    moviesDefer.resolve([{
+	    	id:1
+	    }, {
+	    	id:2
+	    }, { 
+	    	id:3
+	    }]);
+
+	    $rootScope.$digest();
 
 	    expect(ctrl.movies.length).toBe(3);
 	  }));
